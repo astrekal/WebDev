af1c4189164f17a6ab9e63ccaf43d480fe7b9faa
astrekal <54997875+astrekal@users.noreply.github.com>
2019-10-01 13:45:36 -0400

    Add files via upload

---
+++ actualwebsiteexpanded/js.js
@@ -0,0 +1,11 @@
+function changeToDark() {
+    alert("Dark mode will be enabled");
+    document.body.style.background = "linear-gradient(#000000, #777777) fixed";
+    document.body.style.backgroundSize = "cover";
+}
+
+function changeToCrabMode() {
+    alert("CRABS!!!");
+    document.body.style.backgroundImage = "url(img/crab.gif)";
+    document.body.style.backgroundSize = "cover";
+}
